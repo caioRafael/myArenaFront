ac7780996e5b9eaa836623c23c5d97859aeefebb
caioRafael <caiorafaelrg@gmail.com>
2023-10-27 10:19:25 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <div className="flex w-full bg-green-300 min-h-screen">
+      <h1>olá mundo</h1>
+      <Button>Teste</Button>
+    </div>
+  )
+}
